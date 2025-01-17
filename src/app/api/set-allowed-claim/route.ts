2e091b9d1db1eb07fa3751090c012c7da2f77866
MatthewBloemke <mattbloemke@gmail.com>
2025-01-16 22:30:10 -0600

    fixed route

--- src/app/api/set-allowed-claim/route.ts
+++ src/app/api/set-allowed-claim/route.ts
@@ -1,14 +1,12 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 import { authAdmin } from "@/firebase/admin";
-import { NextApiRequest } from "next";
 
-export async function POST(request: NextApiRequest) {
+export async function POST(request: NextRequest) {
   try {
-    const authHeader = request.headers.authorization;
-    const authToken = authHeader?.split(" ")[1];
-  
+    const authToken = request.headers.get("Authorization")?.split(" ")[1];
+
     if (!authToken) {
-      return NextResponse.json({ error: "Unauthorized" }, {status: 401});
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
     const decodedToken = await authAdmin.verifyIdToken(authToken);
@@ -16,15 +14,17 @@ export async function POST(request: NextApiRequest) {
       return NextResponse.json({ error: "Forbidden - Admin access only" }, { status: 403 });
     }
 
-    const { email } = await request.body;
-    if (!email) {
+    try {
+      const { email } = await request.json();
+      const user = await authAdmin.getUserByEmail(email);
+      await authAdmin.setCustomUserClaims(user.uid, { allowed: true });
+  
+      return NextResponse.json({ message: `Access granted to ${email}` }, { status: 200 });
+    } catch {
+
       return NextResponse.json({ error: "Email is required" }, { status: 400 });
     }
 
-    const user = await authAdmin.getUserByEmail(email);
-    await authAdmin.setCustomUserClaims(user.uid, { allowed: true });
-
-    return NextResponse.json({ message: `Access granted to ${email}` }, { status: 200 });
   } catch (error) {
     console.error("Error setting custom claim:", error);
     return NextResponse.json({ error: "Failed to set custom claim" }, { status: 500 });
