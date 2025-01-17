5008b098b10b0239eb03ee687f2ef5a1ffacf849
MatthewBloemke <mattbloemke@gmail.com>
2025-01-16 22:02:10 -0600

    fixed route to use regular response object instead of nextresponse

--- src/app/api/generate-sign-up-link/route.ts
+++ src/app/api/generate-sign-up-link/route.ts
@@ -1,36 +1,35 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { authAdmin } from "@/firebase/admin";
+import { authAdmin } from "@/firebase/admin";
 
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method !== "POST") {
-//     return res.status(405).json({ error: "Method not allowed" });
-//   }
+// POST handler for the API route
+const POST = async (req: Request) => {
+  try {
+    const token = req.headers.get("authorization")?.split(" ")[1];
+    if (!token) {
+      return new Response(null, {status: 401, statusText: 'Unauthorized'});
+    }
 
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1];
-//     if (!token) {
-//       return res.status(401).json({ error: "Unauthorized" });
-//     }
+    // Verify ID token and check if the user has an 'admin' claim
+    const decodedToken = await authAdmin.verifyIdToken(token);
+    if (!decodedToken.admin) {
+      return new Response(null, {status: 403, statusText: 'Forbidden - Admins only'});
+    }
 
-//     // Verify ID token and check if the user has an 'admin' claim
-//     const decodedToken = await authAdmin.verifyIdToken(token);
-//     if (!decodedToken.admin) {
-//       return res.status(403).json({ error: "Forbidden - Admins only" });
-//     }
+    const body = await req.json();
+    const { email } = body;
+    if (!email) {
+      return new Response(null, {status: 400, statusText: 'Email is required'});
+    }
 
-//     const { email } = req.body;
-//     if (!email) {
-//       return res.status(400).json({ error: "Email is required" });
-//     }
+    const signUpLink = await authAdmin.generateSignInWithEmailLink(email, {
+      url: "http://localhost:3000/signin",
+      handleCodeInApp: true,
+    });
 
-//     const signUpLink = await authAdmin.generateSignInWithEmailLink(email, {
-//       url: "http://localhost:3000/signin",
-//       handleCodeInApp: true,
-//     });
+    return new Response(JSON.stringify({link: signUpLink}));
+  } catch (error) {
+    console.error("Error generating sign-up link:", error);
+    return new Response(null, {status: 500, statusText: error instanceof Error ? error.message : String(error)})
+  }
+}
 
-//     res.status(200).json({ link: signUpLink });
-//   } catch (error) {
-//     console.error("Error generating sign-up link:", error);
-//     res.status(500).json({ error: "Failed to generate sign-up link", details: error instanceof Error ? error.message : String(error) });
-//   }
-// }
+export default POST;
